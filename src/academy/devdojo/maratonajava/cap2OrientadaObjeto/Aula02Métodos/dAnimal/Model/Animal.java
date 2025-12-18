1619309aa1ce0e73caa7346d1378171f00488f22
brunocghisi <brunocostaghisi@gmail.com>
2025-12-18 17:15:18 -0300

    Animal: - new method exiberFicha. SOUTs everything about the animal - new logic for setIsSexuado - new logic for isExtinto

--- src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Model/Animal.java
+++ src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Model/Animal.java
@@ -7,21 +7,33 @@ public class Animal {
     private boolean isSexuado;
     private boolean extinto;
 
-    public void imprime()
+    public void exiberFicha()
     {
-        System.out.println("Especie: " + especie);
+        System.out.println("Espécie: " + especie);
         System.out.println("Quantidade: " + quantidade);
         System.out.println("CapacidadeReprodutiva: " + capacidadeReprodutiva);
         System.out.println("Tipo de Reprodução: " + (isSexuado ? "Sexuado" : "Assexuado"));
         System.out.println("Extinto: " + (extinto ? "Extinto" : "Na natureza"));
     }
 
-    public void setEspecie(String especie) {this.especie = especie;}
-    public void setQuantidade(int quantidade) {this.quantidade = quantidade;}
-    public void setCapacidadeReprodutiva(int capacidadeReprodutiva) {this.capacidadeReprodutiva = capacidadeReprodutiva;}
-    public void setExtinto(boolean extinto) {
-        this.extinto = extinto;
+    public void setEspecie(String especie) {
+        this.especie = especie;
     }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+        this.extinto = quantidade <= 0;
+    }
+
+    public void setCapacidadeReprodutiva(int capacidadeReprodutiva) {
+        this.capacidadeReprodutiva = capacidadeReprodutiva;
+    }
+
+    public void setIsSexuado(String isSexuado) {
+        isSexuado = isSexuado.toLowerCase();
+        this.isSexuado = isSexuado.equals("sim");
+    }
+
 
 
 }
--- src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Service/AnimalIndex.java
+++ src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Service/AnimalIndex.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratonajava.cap2OrientadaObjeto.Aula02Métodos.dAnimal.Service;
 
+import academy.devdojo.maratonajava.cap2OrientadaObjeto.Aula02Métodos.dAnimal.Model.Animal;
+
 public class AnimalIndex {
+    public static void main(String[] args) {
+        Animal tucano = new Animal();
+        tucano.setEspecie("Tucano");
+        tucano.setQuantidade(2);
+        tucano.setCapacidadeReprodutiva(2);
+        tucano.setIsSexuado("Sim");
+        tucano.exiberFicha();
+
+
+    }
 }
