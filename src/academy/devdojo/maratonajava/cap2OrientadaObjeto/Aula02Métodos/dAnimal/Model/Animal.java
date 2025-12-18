067dec131016280eb64f3641717132bf52bc9bfb
brunocghisi <brunocostaghisi@gmail.com>
2025-12-18 16:15:35 -0300

    New Class Animal

--- src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Model/Animal.java
+++ src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02Métodos/dAnimal/Model/Animal.java
@@ -9,25 +9,16 @@ public class Animal {
 
     public void imprime()
     {
-        String auxExtinto = extinto ? "Extinto" : "Na natureza";
-        String auxIsSexuado = isSexuado ? "Sexuado" : "Assexuado";
-
         System.out.println("Especie: " + especie);
         System.out.println("Quantidade: " + quantidade);
         System.out.println("CapacidadeReprodutiva: " + capacidadeReprodutiva);
-        System.out.println("Tipo de Reprodução: " + isSexuado ? "Sexuado" : "Assexuado");
-        System.out.println("Extinto: " + auxExtinto);
+        System.out.println("Tipo de Reprodução: " + (isSexuado ? "Sexuado" : "Assexuado"));
+        System.out.println("Extinto: " + (extinto ? "Extinto" : "Na natureza"));
     }
 
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-    public void setCapacidadeReprodutiva(int capacidadeReprodutiva) {
-        this.capacidadeReprodutiva = capacidadeReprodutiva;
-    }
+    public void setEspecie(String especie) {this.especie = especie;}
+    public void setQuantidade(int quantidade) {this.quantidade = quantidade;}
+    public void setCapacidadeReprodutiva(int capacidadeReprodutiva) {this.capacidadeReprodutiva = capacidadeReprodutiva;}
     public void setExtinto(boolean extinto) {
         this.extinto = extinto;
     }
