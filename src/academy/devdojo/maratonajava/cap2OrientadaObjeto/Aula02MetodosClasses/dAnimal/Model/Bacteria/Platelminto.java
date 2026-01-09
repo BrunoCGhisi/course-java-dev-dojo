fd9ff58e143e195804f31e65a8f69e46e3d4e89e
brunocghisi <brunocostaghisi@gmail.com>
2026-01-09 13:51:24 -0300

    Animal Platelminto reproductions
    - Platelminto sexual and asexual reproduction

--- src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02MetodosClasses/dAnimal/Model/Bacteria/Platelminto.java
+++ src/academy/devdojo/maratonajava/cap2OrientadaObjeto/Aula02MetodosClasses/dAnimal/Model/Bacteria/Platelminto.java
@@ -7,11 +7,32 @@ public class Platelminto extends Animal implements ReprodutorAssexuado {
 
     @Override
     public void reproduzirSexuado(int especimesReproduzir){
-        System.out.println("Reproduzindo");
+
+        if (!this.extinto && this.quantidade > 1 && especimesReproduzir > 1 ){
+            int casais = especimesReproduzir / 2;
+
+            this.quantidade = this.quantidade+(casais*this.capacidadeReprodutiva);
+            System.out.println("Cada casal ("+ casais +") de " + this.especie + " reproduziu "+ this.capacidadeReprodutiva);
+            System.out.println("Nova população: " + this.quantidade);
+        }
+        else{
+            System.out.println("O animal não tem população suficiente :/ ");
+        }
+        divider();
+
     }
 
     @Override
     public void reproduzirBiparticao(int especimesReproduzir){
-        this.quantidade = (especimesReproduzir*2);
+        if (!this.extinto && especimesReproduzir > 0 ) {
+            this.quantidade = this.quantidade + (especimesReproduzir * 2);
+            System.out.println("Cada indivíduo de " + this.especie + " duplicou " + especimesReproduzir);
+            System.out.println("Nova população: " + this.quantidade);
+            divider();
+        }
+        else{
+            System.out.println("O animal não tem população suficiente :/ ");
+            divider();
+        }
     }
 }
